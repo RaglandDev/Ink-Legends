9628169b6eb8a452220a7c97d80eb6cf5a1b9994
Alex Ragland <74025483+RaglandDev@users.noreply.github.com>
2025-02-08 20:08:45 -0800

    camera zoom

--- Ink Legends/PlayerCamera.gd
+++ Ink Legends/PlayerCamera.gd
@@ -2,14 +2,19 @@ extends Camera3D
 
 const THRESHOLD = 50
 const STEP = .5
+const ZOOM_STEP = 2
+
+var zoom = 0
 
 @onready var viewportSize = get_viewport().size
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ZoomIn"):
-		self.fov -= 10
-	if Input.is_action_just_pressed("ZoomOut"):
-		self.fov += 10
+	if Input.is_action_just_pressed("ZoomIn") and zoom < 3:
+		position = position - Vector3(ZOOM_STEP, ZOOM_STEP, ZOOM_STEP)
+		zoom += 1
+	if Input.is_action_just_pressed("ZoomOut") and zoom > -10:
+		position = position + Vector3(ZOOM_STEP, ZOOM_STEP, ZOOM_STEP)
+		zoom -= 1
 	var localMousePos = get_viewport().get_mouse_position()
 	edgeScroll(localMousePos.x, localMousePos.y)
 
